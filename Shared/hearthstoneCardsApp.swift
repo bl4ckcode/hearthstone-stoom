16cc00c9113496d7aacdf3296202f7fd2ec74f2c
Carlos Alves <carlosaugusto2005@hotmail.com>
2024-04-03 11:20:36 -0300

    Initial Commit

---
+++ Shared/hearthstoneCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hearthstoneCardsApp.swift
+//  Shared
+//
+//  Created by Carlos alves on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct hearthstoneCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
